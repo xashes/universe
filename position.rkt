469a4201a415dc17acbcfeddf7ac683a4c4ca033
xashes <kellerx0319@gmail.com>
2019-06-24 22:25:23 +0800

    move/random

--- position.rkt
+++ position.rkt
@@ -1,5 +1,10 @@
 #lang typed/racket
 
+(require math)
+
+(provide [struct-out posn])
+(provide move
+         move/random)
 
 (struct posn ([x : Real] [y : Real]) #:transparent)
 
@@ -11,6 +16,22 @@
          [ny (+ oy dy)]
          )
     (posn nx ny)))
+
+; random walk
+(: move/random (-> posn posn))
+(define (move/random p)
+  (let ([choice : Integer (random 4)]
+        [x : Real (posn-x p)]
+        [y : Real (posn-y p)]
+        )
+    (posn
+     (cond
+       [(= choice 0) (add1 x)]
+       [(= choice 1) (sub1 x)]
+       [(= choice 2) (add1 y)]
+       [else (sub1 y)]
+       ) y))
+  )
 
 (module+ test
 
---
+++ world.rkt
@@ -0,0 +1,7 @@
+#lang typed/racket
+
+(require typed/2htdp/universe
+         typed/2htdp/image)
+
+(require "position.rkt")
+
