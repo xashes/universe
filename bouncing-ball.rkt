4691d726d9e2ab86ec40b85450a833095241f8f7
xashes <kellerx0319@gmail.com>
2019-06-27 23:56:15 +0800

    runable

--- bouncing-ball.rkt
+++ bouncing-ball.rkt
@@ -1,67 +1,64 @@
-#lang typed/racket
+#lang racket
 
-(require typed/2htdp/universe
-         typed/2htdp/image)
+(require 2htdp/image
+         2htdp/universe)
 
 (require math)
 
-(struct ball ([loc : (Array Real)] [vel : (Array Real)]) #:transparent #:mutable)
+; WorldState ::= Location
 
 ; constants
 (define WIDTH 600)
 (define HEIGHT 200)
 (define MTS (empty-scene WIDTH HEIGHT))
 
-(define VEL : (Array Real) (array #[1 3]))
+(define LOC0 (array #[(/ WIDTH 2) (/ HEIGHT 2)]))
 
-(define BALL0 (ball (array #[0 0])
-                    VEL))
 (define BALL-PIC (circle 20 'solid 'red))
 
-(: render (-> ball Image))
-(define (render b)
-  (let* ([loc : (Array Real) (ball-loc b)]
-         [x (array-ref loc #(0))]
-         [y (array-ref loc #(1))]
-         )
-    (place-image BALL-PIC
-                 x y
-                 MTS))
+(define VEL (array->mutable-array (array #[10 30])))
+
+(define (render loc)
+  (place-image BALL-PIC
+               (array-ref loc #(0))
+               (array-ref loc #(1))
+               MTS)
   )
 (module+ test
 
-  (require typed/rackunit typed/rackunit/text-ui)
+  (require rackunit rackunit/text-ui)
 
-  (check-equal? (render BALL0)
+  (check-equal? (render LOC0)
                 (place-image BALL-PIC
-                             0 0
+                             (/ WIDTH 2) (/ HEIGHT 2)
                              MTS))
   )
 
-(: move (-> ball ball))
-(define (move b)
-  (let* ([loc : (Array Real) (ball-loc b)]
-         [x (array-ref loc #(0))]
-         [y (array-ref loc #(1))]
-         [vel : (Array Real) (ball-vel b)]
-         [vx (array-ref vel #(0))]
-         [vy (array-ref vel #(1))]
-         )
-    (cond
-      [(or (< x 0) (> x WIDTH)) (begin (set-ball-vel! b (array #((- vx) vy)))
-                                       (ball (array+ loc vel) (ball-vel b)))]
-      [(or (< y 0) (> y HEIGHT)) (begin (set-ball-vel! b (array #(vx (- vy))))
-                                        (ball (array+ loc vel) (ball-vel b)))]
-      [else (ball (array+ loc vel)
-                  vel)]))
+(define (move loc vel)
+  (array+ loc vel)
   )
 
-(: run (-> ball ball))
-(define (run b)
-  (big-bang b : ball
-            [on-tick move]
+(define (redirect v idx)
+  (array-set! v idx (- (array-ref v idx)))
+  )
+
+; (-> Loc Loc)
+(define (tick loc)
+  (let ([x (array-ref loc #(0))]
+        [y (array-ref loc #(1))])
+    (cond
+      [(or (< x 0) (> x WIDTH)) (begin (redirect VEL #(0))
+                                       (move loc VEL))]
+      [(or (< y 0) (> y HEIGHT)) (begin (redirect VEL #(1))
+                                        (move loc VEL))]
+      [else (move loc VEL)]))
+  )
+
+(define (run loc)
+  (big-bang loc
+            [on-tick tick]
             [to-draw render]
             )
   )
 
-(run BALL0)
+(run LOC0)
