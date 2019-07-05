7251eda924abf0535ac69ed7854edebe548e763d
xashes <kellerx0319@gmail.com>
2019-07-05 17:20:30 +0800

    add force

--- mover.rkt
+++ mover.rkt
@@ -13,66 +13,72 @@
 
 ; class
 (define/contract mover%
-  (class/c [get-location (->m (vectorof real?))]
-           [get-velocity (->m (vectorof real?))]
-           [get-acceleration (->m (vectorof real?))]
+  (class/c [update-target (->m (vectorof real?) void?)]
            [update (->m void?)]
+           [figure (->m image?)]
            [render (->m image? image?)]
+           [apply-force (->m (vectorof real?) void?)]
            )
   (class object%
-    (init location velocity acceleration)
+    (init-field [location (vector (/ WIDTH 2) (/ HEIGHT 2))]
+                [velocity #(0 0)]
+                [acceleration #(0 0)]
+                [mass 10]
+                [color 'red]
+                [top-speed 6]
+                )
 
-    (define current-location location)
-    (define current-velocity velocity)
-    (define current-acceleration acceleration)
-    (define img (circle 20 'solid 'red))
-    (define max-vel 10)
+    (define target (vector 200 200))
 
     (super-new)
 
-    (define/public (get-location)
-      current-location)
+    (define (figure)
+      (circle mass 'solid color)
+      )
 
-    (define/public (get-velocity)
-      current-velocity)
+    (define/public (update-target vec)
+      (set! target vec)
+      )
 
-    (define/public (get-acceleration)
-      current-acceleration)
+    (define/private (target-vec)
+      (vector-map - target location)
+      )
 
-    (define/public (get-img)
-      img)
+    (define/private (acc-mag)
+      (vector-magnitude (target-vec))
+      )
+
+    ; -> real?
+    (define/private (gravity)
+      (/ 6
+         (+ (acc-mag) 1)))
+
+    ; -> void?
+    (define/private (update-acc)
+      (set! acceleration (vector-scale (vector-normalize (target-vec)) (gravity)))
+      )
+
+    (define/public (apply-force f)
+      (let ([acc (/ f mass)])
+        (set! acceleration (vector-map + acc acceleration)))
+      )
 
     ; motion algorithm
     (define/public (update)
-      (set! current-acceleration (vector-normalize (random-nd 2 (uniform-dist -1 1))))
-      (set! current-location (vector-map + current-location current-velocity))
-      (set! current-velocity (vector-limit (vector-map + current-velocity current-acceleration) max-vel))
+      (update-acc)
+      (set! velocity (vector-limit (vector-map + velocity acceleration) top-speed))
+      (set! location (vector-map + location velocity))
+      (set! acceleration #(0 0))
       )
 
     (define/public (render bg)
-      (let-values ([(x y) (vector->values current-location)])
+      (let-values ([(x y) (vector->values location)])
         (let ([x (modulo (exact-floor x) WIDTH)]
               [y (modulo (exact-floor y) HEIGHT)])
-          (place-image img
+          (place-image (figure)
                        x y
                        bg
                        ))))))
-
-(module+ test
-  (require rackunit rackunit/text-ui)
-
-  (define m (new mover%
-                 [location (vector 0 0)]
-                 [velocity (vector 10 10)]
-                 [acceleration (vector 20 30)]))
-  (send m update)
-  (check-equal? (send m get-location) #(10 10))
-  (check-equal? (send m render MTS)
-                (place-image (send m get-img)
-                             10 10
-                             MTS
-                             ))
-  )
 
 ; WorldState ::= mover%
 (provide (contract-out [tick (-> (is-a?/c mover%) (is-a?/c mover%))]))
@@ -86,10 +92,18 @@
   (send m render MTS)
   )
 
+(provide (contract-out [mouse-hd (-> (is-a?/c mover%) integer? integer? mouse-event? (is-a?/c mover%))]))
+(define (mouse-hd m x y me)
+  (let ([target (vector x y)])
+    (send m update-target target)
+    m)
+  )
+
 (define/contract (simulate m0)
   (-> (is-a?/c mover%) (is-a?/c mover%))
   (big-bang m0
             [to-draw render]
+            [on-mouse mouse-hd]
             [on-tick tick]
             )
   )
